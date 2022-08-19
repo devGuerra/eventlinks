9eb9429fb70c21963a54ac916baa819dd4681611
Roberto Guerra <prosaka@hotmail.com>
2022-08-19 00:38:45 -0300

    feat: set name shows.bio

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,25 +1,26 @@
 /** @type {import('next-seo').DefaultSeoProps} */
 const defaultSEOConfig = {
-  title: "nextarter-chakra",
-  titleTemplate: "%s | nextarter-chakra",
-  defaultTitle: "nextarter-chakra",
-  description: "Next.js + chakra-ui + TypeScript template",
-  canonical: "https://nextarter-chakra.sznm.dev",
+  title: 'Shows.bio',
+  titleTemplate: '%s | Shows.bio',
+  defaultTitle: 'Shows.bio',
+  description:
+    'Promote your shows on MyShows.bio, Create your profile and share your shows with fans.',
+  canonical: 'https://shows.bio',
   openGraph: {
-    url: "https://nextarter-chakra.sznm.dev",
-    title: "nextarter-chakra",
-    description: "Next.js + chakra-ui + TypeScript template",
+    url: 'https://shows.bio',
+    title: 'shows.bio',
+    description: 'Shows.bio - Share your shows with fans',
     images: [
       {
-        url: "https://og-image.sznm.dev/**nextarter-chakra**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250",
-        alt: "nextarter-chakra.sznm.dev og-image",
+        url: 'https://og-image.sznm.dev/**nextarter-chakra**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250',
+        alt: 'shows.bio og-image',
       },
     ],
-    site_name: "nextarter-chakra",
+    site_name: 'Shows.bio',
   },
   twitter: {
-    handle: "@sozonome",
-    cardType: "summary_large_image",
+    handle: '@dev_guerra',
+    cardType: 'summary_large_image',
   },
 };
 
